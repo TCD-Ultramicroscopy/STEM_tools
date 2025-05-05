d7f7159dd986b37daa404ca183bf1336f19c5fc4
VasilyLebedev <lebedevv@tcd.ie>
2025-05-05 12:15:10 +0100

    Readme amended

--- abTEM_simulations/abtem_run_v01.py
+++ abTEM_simulations/abtem_run_v01.py
@@ -3,33 +3,6 @@
 __author__ = "Vasily A. Lebedev"
 __license__ = "GPL-v3"
 
-'''
-#This code meant to:
-#
-# - import crystallographic data from a given cif file
-# - expand the lattice to create a large set (superblock) of atoms
-# - rotate a superblock to direct a given uvw (or normal to hkl, depends on settings) along Z
-# - crop a superblock to the given 'lamella' sizes
-# - perform a simulation using abTEM routines
-#
-#The main benefit of this code is the possibility to deal with non-orthogonal space groups
-#Trigonal symmentry has not been assessed yet; looking forward to hear feedback on that! 
-#
-#Known issues and TODO:
-# - first frame is simulated separately from frozen phonons, and this simulation is just repeated later on.
-#		maybe add a flag?
-# - dry_run should be implemented as a flag in a full_run
-# - separated lib file to be created
-# - gaussian blur is not handling borders correctly
-# - BF images to be confirmed
-# - in-plane rotation to add 'this hkl up' functionality
-# - check imported libraries
-#
-#Acknowledgements:
-# - Julie M. Bekkevold for the invalueable help with ab-initio simulations and guidance with abTEM code
-# - Project SFI/21/US/3785 for financial support
-'''
-
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 
